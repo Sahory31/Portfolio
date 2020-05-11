8fce4953f825e38986965ea08849d4b3ee970cc8
Karla <karla.alvarez@michelada.io>
2020-05-11 02:03:59 -0500

    Modal window in wich it says that my portfolio is under construction

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
-import CoverPage from './components/header.js'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import CoverPage from './components/header.js';
 
 
 function App() {
     return (
+      <div>
       <CoverPage />
+      </div>
     );
 }
   
