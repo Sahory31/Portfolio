1296d12190a154e01cac022eaf9799974e236777
Karla <karla.alvarez@michelada.io>
2020-06-17 23:58:27 -0500

    I made some changes on the design and titles of my portfolio, before I went to sleep

--- src/components/about.js
+++ src/components/about.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import PetsIcon from '@material-ui/icons/Pets';
+import SchoolIcon from '@material-ui/icons/School';
+import EmojiPeopleIcon from '@material-ui/icons/EmojiPeople';
 import '../styles/About.css';
 
 
@@ -8,13 +10,16 @@ function About(){
     return (
         <div className='informationAboutMe'>
             <div className='description'>
-                <h1>About Me:</h1>
+                <h1><EmojiPeopleIcon style={{fontSize: 50, color:'#002001'}}/>About Me:</h1>
                 <p>
                     Dynamic and energetic Front-end developer, from Guadalajara, Mexico. 
-                    I am always open to challenges and new learning. 
+                    I am always open to challenges and new learning,
                     Passionate about dancing and kittens  .
                 <PetsIcon style={{fontSize: 20}}/>
                 </p>
+            </div>
+            <div className='education'>
+                <h1><SchoolIcon style={{fontSize: 50, color:'#002001'}}/>Educational background</h1>
             </div>
         </div>
     )
