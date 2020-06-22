19e16b92b3a4634657b6812bf37ae47bc8f2c4c4
Karla <karla.alvarez@michelada.io>
2020-06-22 00:12:48 -0500

    I added information about me & change some colors and sizes

--- src/components/about.js
+++ src/components/about.js
@@ -2,6 +2,8 @@ import React from 'react';
 import PetsIcon from '@material-ui/icons/Pets';
 import SchoolIcon from '@material-ui/icons/School';
 import EmojiPeopleIcon from '@material-ui/icons/EmojiPeople';
+import BuildIcon from '@material-ui/icons/Build';
+import CodeIcon from '@material-ui/icons/Code';
 import '../styles/About.css';
 
 
@@ -20,6 +22,25 @@ function About(){
             </div>
             <div className='education'>
                 <h1><SchoolIcon style={{fontSize: 50, color:'#002001'}}/>Educational background</h1>
+                <p className='Laboratoria'>
+                    Laboratoria Guadalajara 2019 -2020 <CodeIcon style={{fontSize: 40, color:'orange'}}/>
+                </p>
+                <p>
+                    Front end development, managing the programming language of JavaScript, html5, CSS, and React.js.
+                </p>
+                <p className='unadm'>
+                    Universidad Abierta y a Distancia de México (UNADM) 2019-2024
+                </p>
+                <p>
+                    Bachelor of Engineering in Software Development
+                </p>
+            </div>
+            <div className='technologies'>
+            <h1><BuildIcon style={{fontSize: 50, color:'#002001'}}/>Technologies</h1>
+
+
+
+            
             </div>
         </div>
     )
