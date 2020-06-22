dc6b1b17c67ef40c9d48875d3254d32dd50703db
Karla <karla.alvarez@michelada.io>
2020-06-22 15:39:13 -0500

    I added the technologies that I learned during the bootcamp

--- src/components/about.js
+++ src/components/about.js
@@ -4,6 +4,14 @@ import SchoolIcon from '@material-ui/icons/School';
 import EmojiPeopleIcon from '@material-ui/icons/EmojiPeople';
 import BuildIcon from '@material-ui/icons/Build';
 import CodeIcon from '@material-ui/icons/Code';
+import  Css from '../Images/css3.png';
+import Cy from '../Images/cypress.png';
+import Git from '../Images/git.png';
+import Html from '../Images/html5.png';
+import Javascript from '../Images/js.png';
+import Material from '../Images/materialUI.png';
+import Node from '../Images/nodejs.png';
+import Reactjs from '../Images/react.png';
 import '../styles/About.css';
 
 
@@ -37,10 +45,38 @@ function About(){
             </div>
             <div className='technologies'>
             <h1><BuildIcon style={{fontSize: 50, color:'#002001'}}/>Technologies</h1>
-
-
-
-            
+            <img 
+                src={Javascript}
+                alt="JavaScript"
+                />
+                <img 
+                src={Html}
+                alt="HTML"
+                />
+                <img 
+                src={Css}
+                alt="Css"
+                />
+                <img 
+                src={Reactjs}
+                alt="Reactjs"
+                />
+                <img 
+                src={Cy}
+                alt="Cypress"
+                />
+                <img 
+                src={Material}
+                alt="MaterialUI"
+                />
+                <img 
+                src={Node}
+                alt="Nodejs"
+                />
+                <img 
+                src={Git}
+                alt="Git"
+                />
             </div>
         </div>
     )
